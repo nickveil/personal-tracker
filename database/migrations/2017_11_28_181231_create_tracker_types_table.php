68284bc9fce332ca1e5aa629e8ae3149a4b0e9eb
nickveil <31191433+nickveil@users.noreply.github.com>
2017-11-28 13:19:44 -0500

    corrected DB order

---
+++ database/migrations/2017_11_28_181231_create_tracker_types_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTrackerTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tracker_types', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('tracker_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tracker_types');
+    }
+}
